3642ad87c1be971bde1da121f792bd9031606d2e
MDMwakulo <mdmwakulo@gmail.com>
2023-05-16 00:09:14 +0300

    Activating Strict Mode

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let hasDriversLisence = false;
+const passTest = true;
+
+if (passTest) hasDriverLisence = true;
+if (hasDriversLisence) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 1997;
