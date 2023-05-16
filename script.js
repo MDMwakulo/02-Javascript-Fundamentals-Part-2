cb6a62a955693f8c8ee5a0088370505b4dcf2559
MDMwakulo <mdmwakulo@gmail.com>
2023-05-16 09:48:38 +0300

    Function Declaration and Function Expression

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 'use strict';
 
-let hasDriversLisence = false;
+/*let hasDriversLisence = false;
 const passTest = true;
 
 if (passTest) hasDriverLisence = true;
 if (hasDriversLisence) console.log('I can drive :D');
 
 // const interface = 'Audio';
-// const private = 1997;
+// const private = 1997;*/
+
+/*function logger() {
+	console.log('My name is Durell');
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	console.log(apples, oranges);
+	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);*/
+
+// Function Declaration and Expression
+function calcAge1(birthYear) {
+	// this is function declaration
+	return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1997);
+const age2 = calcAge2(1997);
+
+const calcAge2 = function(birthYear) {
+	return 2037 - birthYear;
+};
+
+console.log(age1, age2);
