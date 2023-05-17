aac803794b254d8462d9ea7c7012b96cb6adaae3
MDMwakulo <mdmwakulo@gmail.com>
2023-05-17 13:53:44 +0300

    Basic Array Operations (Method)

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ console.log(checkWinner(85, 54, 41, 23, 34, 27));
 console.log(checkWinner(26, 1, 1, 65, 54, 49));*/
 
 // Introduction to Arrays
-const friends = ['Michael', 'Steven', 'Peter'];
+/*const friends = ['Michael', 'Steven', 'Peter'];
 console.log(friends);
 
 const y = new Array(1991, 1984, 2008, 2020);
@@ -151,4 +151,38 @@ const age3 = calcAge(years[years.length - 1]);
 console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
-console.log(ages);
+console.log(ages);*/
+
+// Basic Arrays Operations (Method)
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Joy');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); //Remove last element
+const popped = friends.pop(); //Remove last element
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // Remvoe first
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+
+if (friends.includes('Steven')) {
+	console.log('You have a friend called Steven');
+} else {
+	console.log('You do not have a friend called Steven');
+}
